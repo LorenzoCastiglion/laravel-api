07b79c072d5cb6cf44555baebad93480302d2f51
Lorenzo Castiglion <lorenzo.castiglion@hotmail.it>
2023-01-17 14:46:28 +0100

    project controller

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{   
+    public function index(){
+        $projects = Project::with('languages','types')->paginate(5);
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
+    }
+
+    public function show($slug){
+        $project = Project::with('languages','types')->where('slug', $slug)->paginate(5);
+        if($project){
+            return response()->json([
+                'success' => true,
+                'results' => $project
+            ]);
+        }else{
+            return response()->json([
+                'success' => false,
+                'results' => 'Non hai progetti'
+            ]);
+        }
+    }
+}
